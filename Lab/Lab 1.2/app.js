da744f15d62e940f8b80bde6da4d3e050dd1671a
MaR <linhttfx15087@funix.edu.vn>
2022-09-12 22:35:59 +0700

    Lab2.7

--- Lab/Lab 1.2/app.js
+++ Lab/Lab 1.2/app.js
@@ -3,28 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  console.log("This always run!");
-  next();
-});
+app.use(adminRoutes);
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send(
-    "<form action = '/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from Express</h1>");
-});
+app.use(shopRoutes);
 
 app.listen(3000);
